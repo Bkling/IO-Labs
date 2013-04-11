aed4c17bf5ad0efeb3577bba55f30a777d6f1736
bkling <bkling@L11715038604.wctc.msft>
2013-04-11 17:32:04 -0500

    Finished Labs 1, 2, and 3 still have to finish lab 4.

--- src/lab3/TextReaderApp.java
+++ src/lab3/TextReaderApp.java
@@ -1,31 +1,23 @@
-package lab2;
+package lab3;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class TextReaderApp {
 
     public static void main(String[] args) {
-
-//      Lab 2 Task 1
         File data = new File(File.separatorChar + "Advanced Java" + File.separatorChar
                 + "Temp" + File.separatorChar + "ContactList.txt");
+        //Lab 3 Task 1
         String line;
         BufferedReader in = null;
-        int lineNo = 1;
+        int lineNo;
         try {
-            Scanner keyboard = new Scanner(System.in);
-            System.out.println("Please enter the index of what file you want to get:");
-            String ind = keyboard.nextLine();
-            int index = Integer.parseInt(ind);
             in = new BufferedReader(new FileReader(data));
             for (lineNo = 1; lineNo < data.length(); lineNo++) {
-                if (lineNo == index) {
+                if (lineNo == 2) {
                     line = in.readLine();
                     String[] pieces = line.split("\\|");
-                    System.out.println("Full Name: " + pieces[1] + " " + pieces[0] + "\nAddress: " + pieces[2]
-                            + ", " + pieces[3] + ", " + pieces[4] + ", " + pieces[5] + "\nPhone: " + pieces[7]
-                            + "\nEmail: " + pieces[6] + "\n");
+                    System.out.println("Record Number: " + lineNo + "\nCity: " + pieces[3]);
                 } else {
                     in.readLine();
                 }
